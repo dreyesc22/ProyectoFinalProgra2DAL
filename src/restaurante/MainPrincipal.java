0b757ceee2171a713d2eb3b0acf230c9d911945a
dreyesc22 <dreyesc22@miumg.edu.gt>
2025-10-23 23:39:23 -0600

    Primer commit - Restaurante (NetBeans)

---
+++ src/restaurante/MainPrincipal.java
@@ -0,0 +1,26 @@
+package restaurante;
+import Conexion.CreateConection;
+import java.sql.SQLException;
+// librerias para manipular datos
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MainPrincipal {
+ public static void main(String[] args) throws SQLException {
+    CreateConection  conexionPostgres = new CreateConection();
+    Connection con= conexionPostgres.getConection();
+      try{
+          Statement smt = con.createStatement();
+          String qry = "insert into public.empleado(nombre,apellido,salario)" + " values ('Marina','Catalan',7550.25)";
+          int filasInsertadas = smt.executeUpdate(qry);
+          System.out.println("Total de Registros insertados: "+filasInsertadas);
+          smt.close();
+          
+      } catch (SQLException e){
+          e.getMessage();
+      }
+    }
+
+}
